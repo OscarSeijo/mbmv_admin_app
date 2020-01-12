598bed61be333bafeef04aa855612b40518ad60d
ffelizp <ffeliz@oriontek.do>
2020-01-12 12:24:13 -0400

    Agregando Flujo de Provincias - Sectores - Areas

---
+++ config/roles.php
@@ -0,0 +1,14 @@
+<?php
+return [
+	'names' => [
+		'ADMIN' => 'Administrador',
+	    'EMBARAZADA' => 'Embarazada',
+	    'ENCARGADA' => 'Encargada',
+	],
+	'ids' => [
+		'ADMIN' => '1',
+	    'EMBARAZADA' => '2',
+	    'ENCARGADA' => '3',
+	]
+    
+];
